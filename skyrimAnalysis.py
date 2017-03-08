d8ca407d00df7c9254ae196548a10fee929a397c
Jordan Day <jordan139@hotmail.co.uk>
2017-03-08 14:45:33 +0000

    tokenized each tweet and numbered tweets within terminal. Also all stop words have been removed from the dataset (as its being run) next step is to remove additional such as links and numbers

--- skyrimAnalysis.py
+++ skyrimAnalysis.py
@@ -4,18 +4,40 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords  	
 from nltk import word_tokenize
 
+'''
+reg ex library form nltk
+
+from __future__ import devision 
+from nltk, re, pprint 
+from nltk import word_tokenoze
+
+			for y in filtWord:
+				output = re.sub(r'\d+', '', word)
+				print x,output
+				x = x + 1 
+				print '\n'
+
+
+'''
+
+
 
 def tokenizeWord():
+	x = 0
 	with open('skyrim.csv', 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
 		for row in reader:
+			
+			#remove stop words from each row 
+			filtWord = [word for word in row if word not in stopwords.words('english')]
+			print x,filtWord
+			x = x + 1 
+			print '\n'
 
 
-			for word in row:
-				#remove stop words from each row 
-				filtWord = [word for word in row if word not in stopwords.words('english')]
-				
-				print '\n'.join(row)
+
+
+	
 
 
 
